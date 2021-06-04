05872080066abc7ecc11d2ae5729af254ac27b18
Teddy Walter <teddywalter2016@gmail.com>
2021-06-04 17:32:01 +0100

    create admin test and force login then list user created

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,12 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['name', 'email']
+
+
+admin.site.register(models.User, UserAdmin)
 # Register your models here.
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,28 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest (TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@connectis.co',
+            password="pass@123"
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@connectis.co",
+            password="pass123",
+            name="Teddy Walter"
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse("admin:core_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
