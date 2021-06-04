6b2fd17c0da4ae89ffe4f4e315e173ab4518f83b
Teddy Walter <teddywalter2016@gmail.com>
2021-06-04 04:13:31 +0100

    custom user model and TDD for creating user

--- app/app/settings.py
+++ app/app/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'core.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
